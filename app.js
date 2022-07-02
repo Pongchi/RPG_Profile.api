00cb427d3227bf650a06df797e4a90bb799ce004
Pongchi <67366101+Pongchi@users.noreply.github.com>
2022-07-02 18:54:00 +0900

    connect MongoDB in app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/rpg_api');
 require('dotenv').config();
+
+const DB = mongoose.connection;
+DB.on('error', () => console.log("DB Connection Failed!"));
+DB.once('open', () => console.log('DB Connected!'));
 
 app.get('/', (req, res) => {
     return res.json({'message':'Hello, World!'});
